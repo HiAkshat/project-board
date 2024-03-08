2ed1bf4b246224e46e967c876ba889530b612ee1
Akshat Gupta <akshatgupta300@gmail.com>
2024-03-08 14:21:13 +0530

    close button added to status input

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import { projectAtom } from "./atom";
 
 import AddIcon from '@mui/icons-material/Add';
 import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
@@ -140,9 +141,12 @@ export default function Home() {
             <button onClick={handleNewStatus} className={`${statusInputVisible ? "hidden" : ""} flex items-center gap-0  px-2 py-1 border-2 border-[#454545] rounded-md`}>
               <span className="status-bg" onClick={() => setStatusInputVisible(true)}>+ new status</span>
             </button>
-            <div className={`${statusInputVisible ? "" : "hidden"} flex`}>
-              <input className="bg-transparent border-2 border-[#454545] text-white outline-none text-sm px-2 py-1 rounded-md rounded-r-none border-r-0" value={statusInputText} onChange={e => setStatusInputText(e.target.value)} placeholder="your status title" type="text" />
-              <button disabled={statusInputText==="" ? true : false} onClick={handleAddNewStatus} className="text-white disabled:text-[#454545] px-2 py-1 border-2 border-[#454545] rounded-md rounded-l-none h-full"><DoneIcon fontSize="small"/></button>
+            <div className={`${statusInputVisible ? "" : "hidden"} flex gap-2`}>
+              <button onClick={() => {setStatusInputVisible(false); setStatusInputText("")}} className="text-red-300"><CloseIcon fontSize="small"/></button>
+              <div className="flex">
+                <input className="bg-transparent border-2 border-[#454545] text-white outline-none text-sm px-2 py-1 rounded-md rounded-r-none border-r-0" value={statusInputText} onChange={e => setStatusInputText(e.target.value)} placeholder="your status title" type="text" />
+                <button disabled={statusInputText==="" ? true : false} onClick={handleAddNewStatus} className="text-white disabled:text-[#454545] px-2 py-1 border-2 border-[#454545] rounded-md rounded-l-none h-full"><DoneIcon fontSize="small"/></button>
+              </div>
             </div>
           </div>
 
